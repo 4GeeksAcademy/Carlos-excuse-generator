3b9d339c5462f19c21d6801bf10cd10c5453b0c3
Carlos03tb <carlostb0318@gmail.com>
2024-08-26 11:37:07 +0000

    ejercicio completado

--- src/app.js
+++ src/app.js
@@ -8,4 +8,26 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  generate();
 };
+
+function generate() {
+  var sustantivos = ["Un perro", "Un niño", "Un hombre"];
+  var adjetivos = ["loco", "tonto", "asustado"];
+  var acciones = ["ha roto mi", "mordió mi", "vomitó mi"];
+  var posesiones = ["coche", "moto", "pierna"];
+
+  var rand_first = Math.floor(Math.random() * sustantivos.length);
+  var rand_second = Math.floor(Math.random() * adjetivos.length);
+  var rand_third = Math.floor(Math.random() * acciones.length);
+  var rand_fourth = Math.floor(Math.random() * posesiones.length);
+
+  document.getElementById("result").innerHTML =
+    sustantivos[rand_first] +
+    " " +
+    adjetivos[rand_second] +
+    " " +
+    acciones[rand_third] +
+    " " +
+    posesiones[rand_fourth];
+}
